e1c9c663b6254a34264e24fa40cad127287b1599
vviital <vviital@gmail.com>
2016-08-13 22:31:52 +0300

    Add gulp to project

---
+++ gulpfile.babel.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import del from 'del';
+import babel from 'gulp-babel';
+import { exec } from 'child_process';
+
+gulp.task('clean', () => del('dist'));
+
+gulp.task('build', ['clean'], () => (
+  gulp.src(['src/**/*.js'])
+    .pipe(babel())
+    .pipe(gulp.dest('dist'))
+));
+
+gulp.task('npm:install', ['build'], (cb) => {
+  exec('npm i -g', (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  })
+});
+
+gulp.task('watch', ['npm:install'], () => (
+  gulp.watch('src/**/*.js', ['npm:install'])
+));
